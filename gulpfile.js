5c73f7d2cfaeb06f16eca0c73828a45faf652207
Rajan, Bharat (Contractor) <Bharat_Rajan@comcast.com>
2017-08-11 01:20:30 -0500

    Update: gulpfile.js with comments

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,67 @@ var minifyInline = require('gulp-minify-inline');
 var htmlclean = require('gulp-htmlclean');
 var runSequence = require('run-sequence');
 
-//deletes /build
+/**
+* @description - Deletes /build directory
+* @callBack
+* @param - none
+* @returns - boolean task success | fail
+*/
 gulp.task('cleanup', function(){
     return( del(['build']) );
 });
 
+/**
+* @description - Moves fonts directory from /app -> /build
+* @callBack
+* @param - none
+* @returns - boolean task success | fail
+*/
 gulp.task('move-fonts', function(){
     return gulp.src(['app/fonts/**/*'])
                .pipe(gulp.dest("build/fonts"));
 });
 
+/**
+* @description - Moves icons directory from /app -> /build
+* @callBack
+* @param - none
+* @returns - boolean task success | fail
+*/
 gulp.task('move-icons', function(){
   return gulp.src(['app/icons/**/*'])
              .pipe(gulp.dest("build/icons"));
 });
 
+/**
+* @description - Moves bower_components directory from /app -> /build
+* @callBack
+* @param - none
+* @returns - boolean task success | fail
+*/
 gulp.task('move-bower_components', function(){
   return gulp.src(['app/bower_components/**/*'])
              .pipe(gulp.dest("build/bower_components"));
 });
 
+/**
+* @description - Moves components directory from /app -> /build
+* @callBack
+* @param - none
+* @returns - boolean task success | fail
+*/
 gulp.task('move-components', function(){
   return gulp.src(['app/components/**/*'])
              .pipe(gulp.dest("build/components"));
 });
 
+/**
+* @description - Takes index.html and bundles up all
+* @description - internal imports
+* @callBack
+* @param - none
+* @returns - boolean task success | fail
+*/
 gulp.task('build-index', function(){
        return gulp.src("app/index.html")
                   .pipe(cachebust({
@@ -51,9 +87,7 @@ gulp.task('build-index', function(){
                   .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['cleanup']);
 gulp.task('build', ['move-fonts', 'move-icons', 'move-bower_components', 'move-components', 'build-index']);
-
 gulp.task('default', function(done) {
     runSequence('cleanup', 'build', function() {
         console.log('Build completed successfully');
